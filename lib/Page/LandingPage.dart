6c0c1934cb602754a07075097ec6d16b3d870167
cahyaw <74125680+cahyawibawa12@users.noreply.github.com>
2022-11-22 03:27:22 +0800

    add slider

--- lib/Page/LandingPage.dart
+++ lib/Page/LandingPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:posyandu/widget/widgets.dart';
@@ -10,6 +11,42 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  final List<Widget> slider = [
+    Container(
+      height: 200,
+      width: 200,
+      color: Colors.blue,
+      child: Text('text1'),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      color: Colors.blue,
+      child: Text('text2'),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      color: Colors.blue,
+      child: Text('text3'),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      color: Colors.blue,
+      child: Text('text4'),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      color: Colors.blue,
+      child: Text('text5'),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -19,9 +56,73 @@ class _LandingPageState extends State<LandingPage> {
           backgroundColor: Colors.transparent,
           body: SafeArea(
               child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text("Namaaaaa")],
+            children: <Widget>[
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
+                        backgroundImage: AssetImage('assets/images/bg.png'),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "SIDUMITA",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
+                          Text("Sistem Informasi Ibu Hamil dan Balita"),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Column(
+                    children: [
+                      CarouselSlider(
+                        items: slider,
+                        carouselController: _controller,
+                        options: CarouselOptions(
+                            autoPlay: true,
+                            enlargeCenterPage: true,
+                            aspectRatio: 2.0,
+                            onPageChanged: (index, reason) {
+                              setState(() {
+                                _current = index;
+                              });
+                            }),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: slider.asMap().entries.map((entry) {
+                          return GestureDetector(
+                            onTap: () => _controller.animateToPage(entry.key),
+                            child: Container(
+                              width: 12.0,
+                              height: 12.0,
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 4.0),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: (Theme.of(context).brightness ==
+                                              Brightness.dark
+                                          ? Colors.white
+                                          : Colors.black)
+                                      .withOpacity(
+                                          _current == entry.key ? 0.9 : 0.4)),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           )),
         )
       ],
